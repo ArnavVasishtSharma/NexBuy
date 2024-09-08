ad15c321f1a7ad9b4c4bb80f4ed91691bb088d91
ArnavVasishtSharma <arnav.vsh007@gmail.com>
2024-09-08 21:41:16 +0530

    make the cart quantity interactive

--- scipts/amazon.js
+++ scipts/amazon.js
@@ -108,7 +108,7 @@
 
         if (matchingItem)
             {
-                matchingItem.quantity += 1
+                matchingItem.quantity += 1;
             } else
             {
                 cart.push({
@@ -116,6 +116,13 @@
                     quantity: 1
                 });
             }
-            console.log(cart);
+
+            let cartQuantity=0;
+
+            cart.forEach((item)=>{
+                cartQuantity+= item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;            
     });
-  })
+  }); 
