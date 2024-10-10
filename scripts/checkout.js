8767ce7e20f066031cb5a463e9625e8ff26f2e13
ArnavVasishtSharma <arnav.vsh007@gmail.com>
2024-10-10 22:32:30 +0530

    created class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from '../scripts/Checkout/orderSummery.js'
 import { renderPaymentSummery } from '../scripts/Checkout/paymentSummery.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummery();
