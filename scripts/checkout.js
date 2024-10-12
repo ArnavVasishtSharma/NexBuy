9789e43b5ed499b31eceb1a9506be26cb74bbb2e
ArnavVasishtSharma <arnav.vsh007@gmail.com>
2024-10-12 09:07:15 +0530

    learn promises and covert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,51 @@ import { renderPaymentSummery } from '../scripts/Checkout/paymentSummery.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(()=>{
-renderOrderSummary();
-renderPaymentSummery();
+Promise.all([
+   new Promise ((resolve)=>{
+      loadProducts(()=>{
+       resolve();
+      });
+   }),
+   new Promise((resolve)=>{
+      loadCart(()=>{
+         resolve();
+      });  
+   })
+
+]).then(()=>{
+   renderOrderSummary();
+   renderPaymentSummery();
 })
+/*
+new Promise ((resolve)=>{
+   loadProducts(()=>{
+    resolve();
+   });
+
+
+}).then(()=>{
+return new Promise((resolve)=>{
+   loadCart(()=>{
+      resolve();
+   });
+});  
+
+
+}).then(()=>{
+   renderOrderSummary();
+   renderPaymentSummery();
+})
+/*
+loadProducts(()=>{
+  loadCart(()=>{
+   renderOrderSummary();
+   renderPaymentSummery();
+  }) 
+
+});
+*/
+
+
